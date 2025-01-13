d338dd9df7e7e507a2ea1cdb082f843670a7f4eb
Patrick Southern <Patrick.Southern@vermont.gov>
2025-01-13 15:37:10 -0500

    progress

---
+++ assets/reps.ts
@@ -0,0 +1,57 @@
+export default [
+    {
+        "exercise": "romaniandeadlift",
+        "band":"L",
+        "reps":"8"
+    },
+    {
+        "exercise": "goodmorningdeadlift",
+        "band":"L",
+        "reps":"8"
+    },
+    {
+        "exercise": "sumodeadlift",
+        "band":"L",
+        "reps":"8"
+    },
+    {
+        "exercise": "seatedrow",
+        "band":"L",
+        "reps":"8"
+    },
+    {
+        "exercise": "overheadpress",
+        "band":"L",
+        "reps":"8"
+    },
+    {
+        "exercise": "bandedsquats",
+        "band":"L",
+        "reps":"8"
+    },
+    {
+        "exercise": "antirotation",
+        "band":"L",
+        "reps":"8"
+    },
+    {
+        "exercise": "chestpress",
+        "band":"L",
+        "reps":"8"
+    },
+    {
+        "exercise": "chestfly",
+        "band":"L",
+        "reps":"8"
+    },
+    {
+        "exercise": "plank",
+        "band":"L",
+        "reps":"8"
+    },
+    {
+        "exercise": "hammercurl",
+        "band":"L",
+        "reps":"8"
+    }
+]
